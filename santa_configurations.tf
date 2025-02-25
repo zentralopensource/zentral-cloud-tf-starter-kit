9eff9ad8d88690f7805d2ea4bf66725269d1b5bd
Éric Falconnier <eric@zentral.com>
2025-02-25 23:07:40 +0100

    Add Santa rule for North Pole Security.
    The Santa sync works better when there is at least one rule to
    distribute.

--- santa_configurations.tf
+++ santa_configurations.tf
@@ -8,3 +8,11 @@ resource "zentral_santa_enrollment" "default" {
   configuration_id      = zentral_santa_configuration.default.id
   meta_business_unit_id = zentral_meta_business_unit.default.id
 }
+
+resource "zentral_santa_rule" "default-north-pole-security" {
+  configuration_id  = zentral_santa_configuration.default.id
+  description       = "Allow North Pole Security as publisher."
+  policy            = "ALLOWLIST"
+  target_identifier = "ZMCG7MLDV9"
+  target_type       = "TEAMID"
+}
