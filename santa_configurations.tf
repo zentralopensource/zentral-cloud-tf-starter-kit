7b85adacf7f5f8894427461c42fa9aa8c26cd76b
Éric Falconnier <eric@zentral.com>
2025-07-28 08:34:58 +0200

    Add Santa CEL rule for `spctl` for gatekeeper

--- santa_configurations.tf
+++ santa_configurations.tf
@@ -16,3 +16,13 @@ resource "zentral_santa_rule" "default-north-pole-security" {
   target_identifier = "ZMCG7MLDV9"
   target_type       = "TEAMID"
 }
+
+resource "zentral_santa_rule" "default-spctl-gatekeeper-block" {
+  configuration_id  = zentral_santa_configuration.default.id
+  custom_message    = "You cannot run spctl with these CLI arguments."
+  description       = "Prevent users from disabling gatekeeper."
+  policy            = "CEL"
+  cel_expr          = "['--global-disable', '--master-disable','--disable', '--add', '--remove'].exists(flag, flag in args) ? BLOCKLIST : ALLOWLIST"
+  target_identifier = "platform:com.apple.spctl"
+  target_type       = "SIGNINGID"
+}
